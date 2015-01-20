83b2ef0ba4a9a7c118c8f39a46fc7e34cfeef190
Dylan <dylan.stank@gmail.com>
2015-01-19 18:20:11 -0700

    updated server.py

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
-import SocketServer
 # coding: utf-8
+import SocketServer
+import os
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Dylan Stankievech
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -29,10 +31,128 @@ import SocketServer
 
 class MyWebServer(SocketServer.BaseRequestHandler):
     
-    def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+	request_lines = []
+	first_line_tokens = []
+	uri = ""
+	server_directory = "www"
+
+	def handle(self):
+		
+		self.data = self.request.recv(1024).strip()
+		self.request_lines = self.data.split("\r\n")
+		valid = self.check_request()
+
+		if (valid):
+			self.uri = self.first_line_tokens[1]
+
+			if ("/../" in self.uri):
+				self.not_found()
+				return
+
+			if (self.uri[0] == "/"):
+				self.uri = self.uri[1:]
+
+			resource = self.server_directory + "/" + self.uri
+			found = False
+
+			if (os.path.isfile(resource)):
+				found = True
+			elif (resource.endswith("/") and os.path.isfile(resource + "index.html")):
+				found = True
+				resource = resource + "index.html"
+			elif (os.path.isfile(resource + "/" + "index.html")):
+				found = True
+				resource = resource + "/" + "index.html"
+
+			if (found):
+				content_type = "Content-Type: "
+				if (resource.endswith(".html")):
+					content_type += "text/html; charset=utf-8"
+				elif (resource.endswith(".css")):
+					content_type += "text/css"
+				elif (resource.endswith(".json")):
+					content_type += "application/json"
+				elif (resource.endswith(".js")):
+					content_type += "application/javascript"
+				elif (resource.endswith(".txt")):
+					content_type += "text/plain; charset=utf-8"
+				elif (resource.endswith(".pdf")):
+					content_type += "application/pdf"
+				elif (resource.endswith(".jpg") or resource.endswith(".jpeg")):
+					content_type += "image/jpeg"
+				else:
+					self.unsupported_media()
+					return
+				
+				f = open(resource)
+ 				contents = f.read()
+
+				self.request.sendall("HTTP/1.1 200 OK\r\n" + content_type + "\r\n\r\n" + contents)
+			else:
+				self.not_found()
+		else:
+			self.bad_request()
+
+	def check_request(self):
+		if (len(self.request_lines) < 1):
+			return False
+	
+		self.first_line_tokens = self.request_lines[0].split(" ")
+		if (len(self.first_line_tokens) != 3):
+			return False
+
+		if (self.first_line_tokens[0] != "GET"):
+			return False
+		
+		return True
+
+
+	def bad_request(self):
+		self.request.sendall("HTTP/1.1 400 Bad Request\r\n\r\n" + 
+		"<!DOCTYPE html>" +
+		"<html>" +
+		"<head>" +
+		"<title>400 Bad Request</title>" +
+        "<meta http-equiv=\"Content-Type\"" +
+        "content=\"text/html;charset=utf-8\"/>" +
+		"</head>" +
+		"<body>" +
+		"<h1>400 Bad Request</h1>" +
+		"</div>" +
+		"</body>" +
+		"</html>")
+
+	def unsupported_media(self):
+		self.request.sendall("HTTP/1.1 415 Unsupported Media Type\r\n\r\n" + 
+		"<!DOCTYPE html>" +
+		"<html>" +
+		"<head>" +
+		"<title>415 Unsupported</title>" +
+        "<meta http-equiv=\"Content-Type\"" +
+        "content=\"text/html;charset=utf-8\"/>" +
+		"</head>" +
+		"<body>" +
+		"<h1>415 Unsupported Media Type</h1>" +
+		"</div>" +
+		"</body>" +
+		"</html>")
+
+	def not_found(self):
+		self.request.sendall("HTTP/1.1 404 Not Found\r\n\r\n" + 
+		"<!DOCTYPE html>" +
+		"<html>" +
+		"<head>" +
+		"<title>404 Not Found</title>" +
+        "<meta http-equiv=\"Content-Type\"" +
+        "content=\"text/html;charset=utf-8\"/>" +
+		"</head>" +
+		"<body>" +
+		"<h1>404 Page Missing</h1>" +
+		"</div>" +
+		"</body>" +
+		"</html>")
+		
+		
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
